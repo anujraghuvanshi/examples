204f43ce977c16b5b62d90f382e15e99e1dc2072
anuj-worksdelight <83955471+anuj-worksdelight@users.noreply.github.com>
2021-12-22 17:09:13 +0530

    Update recommendation_model_launcher_keras.py

--- tensorflow_examples/lite/model_maker/third_party/recommendation/ml/model/recommendation_model_launcher_keras.py
+++ tensorflow_examples/lite/model_maker/third_party/recommendation/ml/model/recommendation_model_launcher_keras.py
@@ -200,11 +200,11 @@ def train_and_eval(model, model_dir, train_input_fn, eval_input_fn,
   eval_dataset = eval_input_fn()
   callbacks = get_callbacks(model, model_dir)
   history = model.fit(
-      x=train_dataset,
-      validation_data=eval_dataset,
-      steps_per_epoch=steps_per_epoch,
-      epochs=epochs,
-      validation_steps=eval_steps,
+      x=train_dataset.repeat(),
+      validation_data=eval_dataset.repeat(),
+      steps_per_epoch=int(8000/batch_size),
+      epochs=25,
+      validation_steps=int(2000/batch_size)),
       callbacks=callbacks)
   tf.get_logger().info(history)
   return model
